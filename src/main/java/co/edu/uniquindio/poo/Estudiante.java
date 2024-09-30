2ae3b0babd78703f8e18c4f02fdad318f9d93b98
Sofiasnta <sofia.santamariar@uqvirtual.edu.co>
2024-09-30 18:04:44 -0500

    Update Estudiante.java

--- src/main/java/co/edu/uniquindio/poo/Estudiante.java
+++ src/main/java/co/edu/uniquindio/poo/Estudiante.java
@@ -1,28 +1,60 @@
 package co.edu.uniquindio.poo;
 
+import java.util.Collection;
+import java.util.LinkedList;
+
 public class Estudiante extends Usuario {
 
-    private String ProgramaAcademico;
+    private String programaAcademico;
+    private int cantidadPrestamos;
+    private Collection<Prestamo> prestamos;
 
-    public Estudiante(String nombre, String cedula, String telefono, String correo, String programaAcademico) {
+    
+    public Estudiante(String nombre, String cedula, String telefono, String correo, String programaAcademico, int cantidadPrestamos) {
         super(nombre, cedula, telefono, correo);
-        ProgramaAcademico = programaAcademico;
+        this.programaAcademico = programaAcademico;
+        this.cantidadPrestamos = 0;
+        prestamos = new LinkedList<>();
     }
+
+
+    public void sumarPrestamos(){
+     this.cantidadPrestamos = this.cantidadPrestamos + 1;
+    }
+
 
     public String getProgramaAcademico() {
-        return ProgramaAcademico;
+        return programaAcademico;
     }
+
 
     public void setProgramaAcademico(String programaAcademico) {
-        ProgramaAcademico = programaAcademico;
+        this.programaAcademico = programaAcademico;
     }
 
-    @Override
-    public String toString() {
-        return "Estudiante [ProgramaAcademico=" + ProgramaAcademico + ", getProgramaAcademico()="
-                + getProgramaAcademico() + ", getNombre()=" + getNombre() + ", getCedula()=" + getCedula()
-                + ", getTelefono()=" + getTelefono() + ", getCorreo()=" + getCorreo() + ", toString()="
-                + super.toString() + ", getClass()=" + getClass() + ", hashCode()=" + hashCode() + "]";
+
+    public Collection<Prestamo> getPrestamos() {
+        return prestamos;
     }
+
+
+    public void setPrestamos(Collection<Prestamo> prestamos) {
+        this.prestamos = prestamos;
+    }
+
+
+    public int getCantidadPrestamos() {
+        return cantidadPrestamos;
+    }
+
+
+    public void setCantidadPrestamos(int cantidadPrestamos) {
+        this.cantidadPrestamos = cantidadPrestamos;
+    }
+
+
+  
+    
+  
 
 }
