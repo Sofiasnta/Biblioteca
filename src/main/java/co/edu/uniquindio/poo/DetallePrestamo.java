28067a51a29746255d5d194569a5f70423572713
Sofiasnta <sofia.santamariar@uqvirtual.edu.co>
2024-09-30 18:03:55 -0500

    Update DetallePrestamo.java

--- src/main/java/co/edu/uniquindio/poo/DetallePrestamo.java
+++ src/main/java/co/edu/uniquindio/poo/DetallePrestamo.java
@@ -1,15 +1,58 @@
 package co.edu.uniquindio.poo;
 
-import java.util.LinkedList;
-
 public class DetallePrestamo {
 
-    private LinkedList<Libro> libros;
-    private Prestamo prestamo;
-    private int cantidadLibro;
-    private double precioAlquiler;
+    private int cantidad;
+    private double subTotal;
+    private Libro libro;
 
+    public DetallePrestamo(int cantidad, Libro libro){
+        this.cantidad=cantidad;
+        this.libro=libro;
+        this.subTotal=0;
+    }
 
+    /**
+     * Metodo que permite calcular el subTotal del prestamo
+     * @param COSTODIA
+     * @param diasDePrestamo
+     * @return subTotal
+     */
+
+    public double calcularsubtotal (double COSTODIA, long diasDePrestamo){
+         subTotal = cantidad * COSTODIA * diasDePrestamo;
+         return subTotal;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public double getSubTotal() {
+        return subTotal;
+    }
+
+    public void setSubTotal(double subTotal) {
+        this.subTotal = subTotal;
+    }
+
+    public Libro getLibro() {
+        return libro;
+    }
+
+    public void setLibro(Libro libro) {
+        this.libro = libro;
+    }
+
+    @Override
+    public String toString() {
+        return "DetallePrestamo [cantidad=" + cantidad + ", subTotal=" + subTotal + ", libro=" + libro + "]";
+    }
+    
+    
+    
 }
- 
-
